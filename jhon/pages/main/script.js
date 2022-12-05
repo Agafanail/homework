6b10866533c47bc0c79b1536293cb8bc46aad0cf
Dzmitry Pauliukevich <dmitripavluk@gmail.com>
2022-12-05 23:36:32 +0300

    add: new files

---
+++ jhon/pages/main/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.menu__icon').click(function(event) {
+        $('.menu__icon, .menu__body').toggleClass('active');
+        $('body').toggleClass('lock')
+    });
+});
+
+
